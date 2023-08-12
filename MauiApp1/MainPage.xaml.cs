c51ba7db04a2012e8fc024ad56899c9c07a19674
tedead <jhalv27@gmail.com>
2023-08-12 18:01:49 -0500

    initial

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿using Plugin.Maui.Audio;
+
+namespace MauiApp1;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+    readonly IAudioManager audioManager;
+
+    IAudioPlayer audioPlayer;
+
+    public async void PlayAudio(string resource)
+    {
+        try
+        {
+            //var audioPlayer = audioManager.CreatePlayer(await FileSystem.OpenAppPackageFileAsync(resource));
+            audioPlayer = AudioManager.Current.CreatePlayer(await FileSystem.OpenAppPackageFileAsync(resource));
+
+
+            if (audioPlayer.IsPlaying)
+            {
+                audioPlayer.Stop();
+            }
+
+            audioPlayer.Play();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+
+    }
+
+    public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		count++;
+
+		//if (count == 1)
+			//CounterBtn.Text = $"Clicked {count} time";
+		//else
+			//CounterBtn.Text = $"Clicked {count} times";
+
+		//SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+
+    private void OnButtonClick(object sender, EventArgs e)
+    {
+        try
+        {
+            Button button = (Button)sender;
+
+            string parameter = button.CommandParameter.ToString();
+
+            if (parameter.ToLowerInvariant().Equals("counter"))
+            {
+                Counter();
+            }
+            else
+            {
+                PlayAudio(parameter);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that might occur while playing the MP3 file.
+            Console.WriteLine($"Error playing MP3: {ex.Message}");
+        }
+    }
+
+    private void Counter()
+    {
+        count++;
+
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
+}
+
---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
+using Plugin.Maui.Audio;
+
+namespace MauiApp1;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder.UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddSingleton(AudioManager.Current);
+
+        builder.ConfigureMauiHandlers(h => h.AddHandler<NamedEntry, EntryHandler>());
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+        return builder.Build();
+    }
+}
